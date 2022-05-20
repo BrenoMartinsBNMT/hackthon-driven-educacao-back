2c739e6db94ebc3de44224168a242817fd7fb9de
breno <brenoamrtinscunha@gmail.com>
2022-05-20 19:30:32 -0300

    teste rota de cadastro

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express, { json } from "express";
 import cors from "cors";
+import authRoutes from "./routers/authRouters.js";
+
 const app = express();
 
 app.use(json());
 app.use(cors());
+app.use(authRoutes);
 
 app.get("/", (req, res) => {
   res.send("api funcionando");

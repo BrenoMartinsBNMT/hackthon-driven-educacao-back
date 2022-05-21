d52f4e098619fa2a55dda0449158c954efc8e330
breno <brenoamrtinscunha@gmail.com>
2022-05-20 22:08:00 -0300

    finalizado up github

--- controllers/GetExamsController.js
+++ controllers/GetExamsController.js
@@ -17,10 +17,29 @@ export async function GetExams(req, res) {
   let { email } = hasloged;
   let { username } = await db.collection("signUp").findOne({ email });
   let exams = await db.collection("exams").find({}).toArray();
-
+  console.log();
+  shuffle(exams[0].alternativas);
   if (!exams) {
     res.send("Ainda não temos nemhuma pergunta :(");
   }
   let gambiarra = { exams, username };
   res.send(gambiarra);
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
 }
